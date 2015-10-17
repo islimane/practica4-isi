c29148a2732591b77c3611b9c5b78a7d8bf83b50
Kaloyan Todorov <k.todorov@outlook.com>
2015-10-17 23:30:19 +0200

    Añadido test para probar el funcionamiento del registro, simula un registro nuevo. Se han añadido atributos class a los anchors en el template main para poder localizarlos.

---
+++ tests/jasmine/client/integration/pruebaRegisterSpec.js
@@ -0,0 +1,64 @@
+
+describe("Making a new register ", function () {
+
+  beforeAll(function(done){
+  	$("a.register").click();
+  	Meteor.logout(function(){
+      Tracker.afterFlush(done);
+    });
+  });
+
+  afterEach(function(done){
+    Meteor.logout(function(){
+      Tracker.afterFlush(done);
+    });
+  });
+
+ it('should be able to find home anchor' , function(){
+    expect($("a.home").length).toBeGreaterThan(0);
+ });
+
+ it('should be able to find register anchor' , function(){
+    expect($("a.register").length).toBeGreaterThan(0);
+ });
+
+ describe ( " Register form" , function () {
+
+ 	$("a.home").click();
+ 	$("a.register").click();
+
+    it("should be able to find email input", function(){
+    	expect($("form.register input.email").length).toBeGreaterThan(0);
+    });
+
+    it("should be able to find password input", function(){
+    	expect($("form.register input.password").length).toBeGreaterThan(0);
+    });
+    
+
+ });
+
+ describe("Registering a new user" , function() {
+
+	it("should the user be registered and logged in", function (done){
+
+	    $("a.register").click();
+
+		var mail = "doomie@gmail.com" ;
+		var pass = "1234567890" ;
+
+		$("form.register input.email").val(mail);
+		$("form.register input.password").val(pass);
+
+		spyOn(Accounts, "createUser").and.callThrough() ;
+
+		$("form.register").submit();
+
+		expect(Accounts.createUser).toHaveBeenCalled();
+		done() ;
+	});
+	
+
+});
+
+});
