343b9f56fbf2492c6494f35dd0751405ca194c2f
Ismael Slimane Zubillaga <isma.bled777@gmail.com>
2015-10-18 15:18:33 +0200

    Tested login and register functionality shouldn't be able

--- tests/jasmine/client/integration/domTestSpec.js
+++ tests/jasmine/client/integration/domTestSpec.js
@@ -6,12 +6,6 @@ describe("DOM elements with user logged in", function () {
     });
   });
 
-  afterEach(function(done){
-    Meteor.logout(function(){
-      Tracker.afterFlush(done);
-    });
-  });
-
   it("should be able to create a new list", function(){
     var addListSubmit = $("[name='listName']");
     expect(addListSubmit.length).toBe(1);
@@ -20,5 +14,12 @@ describe("DOM elements with user logged in", function () {
   it("should be able to logout", function(){
     var logoutAnchor = $("[class='logout']");
     expect(logoutAnchor.length).toBe(1);
+  });
+
+  it("shouldn't be able to login nor register", function(){
+    var loginAnchor = $("a.login");
+    var registertAnchor = $("a.register");
+    var length = loginAnchor.length + registertAnchor.length;
+    expect(length).toBe(0);
   });
 });
