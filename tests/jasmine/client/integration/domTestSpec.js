672cb64642b0e80666bedfef87e4b714e5e1e1e7
Ismael Slimane Zubillaga <isma.bled777@gmail.com>
2015-10-19 21:38:53 +0200

    Tested DOM when a user is not logged in

--- tests/jasmine/client/integration/domTestSpec.js
+++ tests/jasmine/client/integration/domTestSpec.js
@@ -6,6 +6,12 @@ describe("DOM elements with user logged in", function () {
     });
   });
 
+  afterEach(function (done){
+    Meteor.logout(function() {
+      Tracker.afterFlush(done);
+    });
+  });
+
   it("should be able to create a new list", function(){
     var addListSubmit = $("[name='listName']");
     expect(addListSubmit.length).toBe(1);
@@ -21,5 +27,31 @@ describe("DOM elements with user logged in", function () {
     var registertAnchor = $("a.register");
     var length = loginAnchor.length + registertAnchor.length;
     expect(length).toBe(0);
+  });
+});
+
+describe("DOM elements with user logged in", function () {
+
+  beforeEach(function (done) {
+    Meteor.logout(function() {
+      Tracker.afterFlush(done);
+    });
+  });
+
+  it("shouldn't be able to create a new list", function(){
+    var addListSubmit = $("[name='listName']");
+    expect(addListSubmit.length).toBe(0);
+  });
+
+  it("shouldn't be able to logout", function(){
+    var logoutAnchor = $("[class='logout']");
+    expect(logoutAnchor.length).toBe(0);
+  });
+
+  it("should be able to login nor register", function(){
+    var loginAnchor = $("a.login");
+    var registertAnchor = $("a.register");
+    var length = loginAnchor.length + registertAnchor.length;
+    expect(length).toBe(2);
   });
 });
