750f8be0f26d6d403c24a5d22c27eba70f375c14
j.peraltar <getajoel@gmail.com>
2015-10-19 21:04:58 +0200

    Added 'List adding' test

---
+++ tests/jasmine/client/integration/functionalitySpec.js
@@ -0,0 +1,27 @@
+describe("List adding", function () {
+  beforeEach(function (done) {
+    Meteor.loginWithPassword("pepe@gmail.com", "123456", function(err){
+      Tracker.afterFlush(done);
+    });
+  });
+
+  afterEach(function (done){
+    Meteor.logout(function() {
+      Tracker.afterFlush(done);
+    });
+  });
+
+  it("should find new added list in the collection", function () {
+    var listName = "listX";
+    var userId = Meteor.userId();
+
+    $("[name=listName]").val(listName);
+    $("form.addList").submit();
+
+    setTimeout(function () {
+      expect(Lists.findOne({name: listName, 
+                            createdBy: userId})).toBeTruthy();
+      done();
+    }, 100);
+  });
+});
